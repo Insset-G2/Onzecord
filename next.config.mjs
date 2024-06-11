4150c07fb973606c5ab94ede4ad668ca3a408b13
Asgarrrr <caruellej@hotmail.fr>
2024-06-11 03:22:43 +0200

    chore: Update next.config.mjs to disable server minification

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        serverMinification: false,
+    },
     reactStrictMode: false,
     images: {
         remotePatterns: [
